cf3fe24ae8b7cc3e02eb961880af40e5c6058d6b
Pramsco <anggapramuja0000@gmail.com>
2024-02-02 10:18:53 +0700

    update

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -108,274 +108,35 @@ class Dashboard extends CI_Controller
     function cek_dokumen_warning()
     {
         $id_vendor = $this->session->userdata('id_vendor');
-        // siup
-        // siup_warning_dokumen
-        $row_siup = $this->M_dashboard->cek_vendor_siup_dokumen($id_vendor);
-        $expiredDate_siup = new DateTime($row_siup['tgl_berlaku']);
-        $currentDate_siup = new DateTime();
-        $timeUntilExpiration_siup = $currentDate_siup->diff($expiredDate_siup);
-        $monthsUntilExpiration_siup = $timeUntilExpiration_siup->format('%m');
-        $weeksUntilExpiration_siup = floor($timeUntilExpiration_siup->days / 7);
-        $daysUntilExpiration_siup =  floor($timeUntilExpiration_siup->days % 7);
-        $yearsUntilExpiration_siup = $expiredDate_siup->format('Y');
-        if ($monthsUntilExpiration_siup == 0) {
-            $monthsUntilExpiration_siup = '';
-            $monthsUntilExpiration_siup_validation = 0;
-        } else {
-            $monthsUntilExpiration_siup = $timeUntilExpiration_siup->format('%m') . ' Bulan';
-            $monthsUntilExpiration_siup_validation = $timeUntilExpiration_siup->format('%m');
-        }
-
-        if ($weeksUntilExpiration_siup == 0) {
-            $weeksUntilExpiration_siup = '';
-            $weeksUntilExpiration_siup_validation = 0;
-        } else {
-            $weeksUntilExpiration_siup = floor($timeUntilExpiration_siup->days / 7) . ' Minggu';
-            $weeksUntilExpiration_siup_validation = floor($timeUntilExpiration_siup->days / 7);
-        }
-
-        if ($daysUntilExpiration_siup == 0) {
-            $daysUntilExpiration_siup = '';
-            $daysUntilExpiration_siup_validation = 0;
-        } else {
-            $daysUntilExpiration_siup =  floor($timeUntilExpiration_siup->days % 7) . ' Hari';
-            $daysUntilExpiration_siup_validation =  floor($timeUntilExpiration_siup->days % 7);
-        }
-        // end_siup_warning_dokumen
-
+        $row_akta_pendirian = $this->M_dashboard->cek_vendor_akta_pendirian_dokumen($id_vendor);
+        $row_sppkp = $this->M_dashboard->cek_vendor_sppkp_dokumen($id_vendor);
+        $row_npwp = $this->M_dashboard->cek_vendor_npwp_dokumen($id_vendor);
 
         // nib
-        // nib_warning_dokumen
         $row_nib = $this->M_dashboard->cek_vendor_nib_dokumen($id_vendor);
-        $expiredDate_nib = new DateTime($row_nib['tgl_berlaku']);
-        $currentDate_nib = new DateTime();
-        $timeUntilExpiration_nib = $currentDate_nib->diff($expiredDate_nib);
-        $monthsUntilExpiration_nib = $timeUntilExpiration_nib->format('%m');
-        $weeksUntilExpiration_nib = floor($timeUntilExpiration_nib->days / 7);
-        $daysUntilExpiration_nib =  floor($timeUntilExpiration_nib->days % 7);
-        $yearsUntilExpiration_nib = $expiredDate_nib->format('Y');
-        if ($monthsUntilExpiration_nib == 0) {
-            $monthsUntilExpiration_nib = '';
-            $monthsUntilExpiration_nib_validation = 0;
-        } else {
-            $monthsUntilExpiration_nib = $timeUntilExpiration_nib->format('%m') . ' Bulan';
-            $monthsUntilExpiration_nib_validation = $timeUntilExpiration_nib->format('%m');
-        }
-
-        if ($weeksUntilExpiration_nib == 0) {
-            $weeksUntilExpiration_nib = '';
-            $weeksUntilExpiration_nib_validation = 0;
-        } else {
-            $weeksUntilExpiration_nib = floor($timeUntilExpiration_nib->days / 7) . ' Minggu';
-            $weeksUntilExpiration_nib_validation = floor($timeUntilExpiration_nib->days / 7);
-        }
-
-        if ($daysUntilExpiration_nib == 0) {
-            $daysUntilExpiration_nib = '';
-            $daysUntilExpiration_nib_validation = 0;
-        } else {
-            $daysUntilExpiration_nib =  floor($timeUntilExpiration_nib->days % 7) . ' Hari';
-            $daysUntilExpiration_nib_validation =  floor($timeUntilExpiration_nib->days % 7);
-        }
-        // end_nib_warning_dokumen
-
         // sbu
-        // sbu_warning_dokumen
         $row_sbu = $this->M_dashboard->cek_vendor_sbu_dokumen($id_vendor);
-        $expiredDate_sbu = new DateTime($row_sbu['tgl_berlaku']);
-        $currentDate_sbu = new DateTime();
-        $timeUntilExpiration_sbu = $currentDate_sbu->diff($expiredDate_sbu);
-        $monthsUntilExpiration_sbu = $timeUntilExpiration_sbu->format('%m');
-        $weeksUntilExpiration_sbu = floor($timeUntilExpiration_sbu->days / 7);
-        $daysUntilExpiration_sbu =  floor($timeUntilExpiration_sbu->days % 7);
-        $yearsUntilExpiration_sbu = $expiredDate_sbu->format('Y');
-        if ($monthsUntilExpiration_sbu == 0) {
-            $monthsUntilExpiration_sbu = '';
-            $monthsUntilExpiration_sbu_validation = 0;
-        } else {
-            $monthsUntilExpiration_sbu = $timeUntilExpiration_sbu->format('%m') . ' Bulan';
-            $monthsUntilExpiration_sbu_validation = $timeUntilExpiration_sbu->format('%m');
-        }
+        // siup
+        $row_siup = $this->M_dashboard->cek_vendor_siup_dokumen($id_vendor);
+        // siujk
+        $row_siujk = $this->M_dashboard->cek_vendor_siujk_dokumen($id_vendor);
+        // skdp
+        $row_skdp = $this->M_dashboard->cek_vendor_skdp_dokumen($id_vendor);
 
-        if ($weeksUntilExpiration_sbu == 0) {
-            $weeksUntilExpiration_sbu = '';
-            $weeksUntilExpiration_sbu_validation = 0;
-        } else {
-            $weeksUntilExpiration_sbu = floor($timeUntilExpiration_sbu->days / 7) . ' Minggu';
-            $weeksUntilExpiration_sbu_validation = floor($timeUntilExpiration_sbu->days / 7);
-        }
-
-        if ($daysUntilExpiration_sbu == 0) {
-            $daysUntilExpiration_sbu = '';
-            $daysUntilExpiration_sbu_validation = 0;
-        } else {
-            $daysUntilExpiration_sbu =  floor($timeUntilExpiration_sbu->days % 7) . ' Hari';
-            $daysUntilExpiration_sbu_validation =  floor($timeUntilExpiration_sbu->days % 7);
-        }
-        // end_sbu_warning_dokumen
-
-        // akta_pendirian
-        // akta_pendirian_warning_dokumen
-        $row_akta_pendirian = $this->M_dashboard->cek_vendor_akta_pendirian_dokumen($id_vendor);
-        $expiredDate_akta_pendirian = new DateTime($row_akta_pendirian['tgl_berlaku_akta']);
-        $currentDate_akta_pendirian = new DateTime();
-        $timeUntilExpiration_akta_pendirian = $currentDate_akta_pendirian->diff($expiredDate_akta_pendirian);
-        $monthsUntilExpiration_akta_pendirian = $timeUntilExpiration_akta_pendirian->format('%m');
-        $weeksUntilExpiration_akta_pendirian = floor($timeUntilExpiration_akta_pendirian->days / 7);
-        $daysUntilExpiration_akta_pendirian =  floor($timeUntilExpiration_akta_pendirian->days % 7);
-        $yearsUntilExpiration_akta_pendirian = $expiredDate_akta_pendirian->format('Y');
-        if ($monthsUntilExpiration_akta_pendirian == 0) {
-            $monthsUntilExpiration_akta_pendirian = '';
-            $monthsUntilExpiration_akta_pendirian_validation = 0;
-        } else {
-            $monthsUntilExpiration_akta_pendirian = $timeUntilExpiration_akta_pendirian->format('%m') . ' Bulan';
-            $monthsUntilExpiration_akta_pendirian_validation = $timeUntilExpiration_akta_pendirian->format('%m');
-        }
-
-        if ($weeksUntilExpiration_akta_pendirian == 0) {
-            $weeksUntilExpiration_akta_pendirian = '';
-            $weeksUntilExpiration_akta_pendirian_validation = 0;
-        } else {
-            $weeksUntilExpiration_akta_pendirian = floor($timeUntilExpiration_akta_pendirian->days / 7) . ' Minggu';
-            $weeksUntilExpiration_akta_pendirian_validation = floor($timeUntilExpiration_akta_pendirian->days / 7);
-        }
-
-        if ($daysUntilExpiration_akta_pendirian == 0) {
-            $daysUntilExpiration_akta_pendirian = '';
-            $daysUntilExpiration_akta_pendirian_validation = 0;
-        } else {
-            $daysUntilExpiration_akta_pendirian =  floor($timeUntilExpiration_akta_pendirian->days % 7) . ' Hari';
-            $daysUntilExpiration_akta_pendirian_validation =  floor($timeUntilExpiration_akta_pendirian->days % 7);
-        }
-        // end_akta_pendirian_warning_dokumen
-
-
-
-        // sppkp
-        // sppkp_warning_dokumen
-        $row_sppkp = $this->M_dashboard->cek_vendor_sppkp_dokumen($id_vendor);
-        $expiredDate_sppkp = new DateTime($row_sppkp['tgl_berlaku']);
-        $currentDate_sppkp = new DateTime();
-        $timeUntilExpiration_sppkp = $currentDate_sppkp->diff($expiredDate_sppkp);
-        $monthsUntilExpiration_sppkp = $timeUntilExpiration_sppkp->format('%m');
-        $weeksUntilExpiration_sppkp = floor($timeUntilExpiration_sppkp->days / 7);
-        $daysUntilExpiration_sppkp = $timeUntilExpiration_sppkp->days % 7;
-        $yearsUntilExpiration_sppkp = $expiredDate_sppkp->format('Y');
-        if ($monthsUntilExpiration_sppkp == 0) {
-            $monthsUntilExpiration_sppkp = '';
-            $monthsUntilExpiration_sppkp_validation = 0;
-        } else {
-            $monthsUntilExpiration_sppkp = $timeUntilExpiration_sppkp->format('%m') . ' Bulan';
-            $monthsUntilExpiration_sppkp_validation = $timeUntilExpiration_sppkp->format('%m');
-        }
-
-        if ($weeksUntilExpiration_sppkp == 0) {
-            $weeksUntilExpiration_sppkp = '';
-            $weeksUntilExpiration_sppkp_validation = 0;
-        } else {
-            $weeksUntilExpiration_sppkp = floor($timeUntilExpiration_sppkp->days / 7) . ' Minggu';
-            $weeksUntilExpiration_sppkp_validation = floor($timeUntilExpiration_sppkp->days / 7);
-        }
-
-        if ($daysUntilExpiration_sppkp == 0) {
-            $daysUntilExpiration_sppkp = '';
-            $daysUntilExpiration_sppkp_validation = 0;
-        } else {
-            $daysUntilExpiration_sppkp =  floor($timeUntilExpiration_sppkp->days % 7) . ' Hari';
-            $daysUntilExpiration_sppkp_validation =  floor($timeUntilExpiration_sppkp->days % 7);
-        }
-        // end_sppkp_warning_dokumen
-
-
-        // npwp
-        // npwp_warning_dokumen
-        $row_npwp = $this->M_dashboard->cek_vendor_npwp_dokumen($id_vendor);
-        $expiredDate_npwp = new DateTime($row_npwp['tgl_berlaku']);
-        $currentDate_npwp = new DateTime();
-        $timeUntilExpiration_npwp = $currentDate_npwp->diff($expiredDate_npwp);
-        $monthsUntilExpiration_npwp = $timeUntilExpiration_npwp->format('%m');
-        $yearsUntilExpiration_npwp = $expiredDate_npwp->format('Y');
-        $weeksUntilExpiration_npwp = floor($timeUntilExpiration_npwp->days / 7);
-        $daysUntilExpiration_npwp = $timeUntilExpiration_npwp->days % 7;
-        if ($monthsUntilExpiration_npwp == 0) {
-            $monthsUntilExpiration_npwp = '';
-            $monthsUntilExpiration_npwp_validation = 0;
-        } else {
-            $monthsUntilExpiration_npwp = $timeUntilExpiration_npwp->format('%m') . ' Bulan';
-            $monthsUntilExpiration_npwp_validation = $timeUntilExpiration_npwp->format('%m');
-        }
-
-        if ($weeksUntilExpiration_npwp == 0) {
-            $weeksUntilExpiration_npwp = '';
-            $weeksUntilExpiration_npwp_validation = 0;
-        } else {
-            $weeksUntilExpiration_npwp = floor($timeUntilExpiration_npwp->days / 7) . ' Minggu';
-            $weeksUntilExpiration_npwp_validation = floor($timeUntilExpiration_npwp->days / 7);
-        }
-
-        if ($daysUntilExpiration_npwp == 0) {
-            $daysUntilExpiration_npwp = '';
-            $daysUntilExpiration_npwp_validation = 0;
-        } else {
-            $daysUntilExpiration_npwp =  floor($timeUntilExpiration_npwp->days % 7) . ' Hari';
-            $daysUntilExpiration_npwp_validation =  floor($timeUntilExpiration_npwp->days % 7);
-        }
-        // end_npwp_warning_dokumen
-
+        
         $response = [
             // siup
             'row_siup' => $row_siup,
-            'validasi_bulan_siup' => $monthsUntilExpiration_siup_validation,
-            'validasi_minggu_siup' => $weeksUntilExpiration_siup_validation,
-            'validasi_hari_siup' => $daysUntilExpiration_siup_validation,
-            'warning_siup' => "Kadaluarsa Dalam $monthsUntilExpiration_siup, $weeksUntilExpiration_siup, dan $daysUntilExpiration_siup.",
-            'nama_dokumen_siup' => 'siup',
-            'tahun_siup' => $yearsUntilExpiration_siup,
-
             // nib
             'row_nib' => $row_nib,
-            'validasi_bulan_nib' => $monthsUntilExpiration_nib_validation,
-            'validasi_minggu_nib' => $weeksUntilExpiration_nib_validation,
-            'validasi_hari_nib' => $daysUntilExpiration_nib_validation,
-            'warning_nib' => "Kadaluarsa Dalam $monthsUntilExpiration_nib, $weeksUntilExpiration_nib, dan $daysUntilExpiration_nib.",
-            'nama_dokumen_nib' => 'nib',
-            'tahun_nib' => $yearsUntilExpiration_nib,
-
             // sbu
             'row_sbu' => $row_sbu,
-            'validasi_bulan_sbu' => $monthsUntilExpiration_sbu_validation,
-            'validasi_minggu_sbu' => $weeksUntilExpiration_sbu_validation,
-            'validasi_hari_sbu' => $daysUntilExpiration_sbu_validation,
-            'warning_sbu' => "Kadaluarsa Dalam $monthsUntilExpiration_sbu, $weeksUntilExpiration_sbu, dan $daysUntilExpiration_sbu.",
-            'nama_dokumen_sbu' => 'sbu',
-            'tahun_sbu' => $yearsUntilExpiration_sbu,
-
             // akta_pendirian
             'row_akta_pendirian' => $row_akta_pendirian,
-            'validasi_bulan_akta_pendirian' => $monthsUntilExpiration_akta_pendirian_validation,
-            'validasi_minggu_akta_pendirian' => $weeksUntilExpiration_akta_pendirian_validation,
-            'validasi_hari_akta_pendirian' => $daysUntilExpiration_akta_pendirian_validation,
-            'warning_akta_pendirian' => "Kadaluarsa Dalam $monthsUntilExpiration_akta_pendirian, $weeksUntilExpiration_akta_pendirian, dan $daysUntilExpiration_akta_pendirian.",
-            'nama_dokumen_akta_pendirian' => 'akta pendirian',
-            'tahun_akta_pendirian' => $yearsUntilExpiration_akta_pendirian,
-
             // sppkp
             'row_sppkp' => $row_sppkp,
-            'validasi_bulan_sppkp' => $monthsUntilExpiration_sppkp_validation,
-            'validasi_minggu_sppkp' => $weeksUntilExpiration_sppkp_validation,
-            'validasi_hari_sppkp' => $daysUntilExpiration_sppkp_validation,
-            'warning_sppkp' => "Kadaluarsa Dalam $monthsUntilExpiration_sppkp, $weeksUntilExpiration_sppkp, dan $daysUntilExpiration_sppkp.",
-            'nama_dokumen_sppkp' => 'sppkp',
-            'tahun_sppkp' => $yearsUntilExpiration_sppkp,
-
             // npwp
             'row_npwp' => $row_npwp,
-            'validasi_bulan_npwp' => $monthsUntilExpiration_npwp_validation,
-            'validasi_minggu_npwp' => $weeksUntilExpiration_npwp_validation,
-            'validasi_hari_npwp' => $daysUntilExpiration_npwp_validation,
-            'warning_npwp' => "Kadaluarsa Dalam $monthsUntilExpiration_npwp, $weeksUntilExpiration_npwp, dan $daysUntilExpiration_npwp.",
-            'nama_dokumen_npwp' => 'npwp',
-            'tahun_npwp' => $yearsUntilExpiration_npwp,
         ];
         $this->output->set_content_type('application/json')->set_output(json_encode($response));
     }
